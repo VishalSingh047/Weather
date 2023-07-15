f2967ed9da9d4bf646306b3300f1831d460e1b35
Vishal Singh <vs997131@gmail.com>
2023-07-15 14:58:58 +0530

    Weather_Detection

---
+++ index.js
@@ -0,0 +1,95 @@
+const api = {
+    key: "2fa73590fd8b5a4c6e68098ad5625395",
+    base: "https://api.openweathermap.org/data/2.5/"
+  };
+  
+  const searchbox = document.querySelector(".search-box");
+  searchbox.addEventListener("keypress", setQuery);
+  
+  function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+  }
+  
+  function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then((weather) => {
+        return weather.json();
+      })
+      .then(displayResults);
+  }
+  
+  
+    function displayResults(weather) {
+      let city = document.querySelector(".location .city");
+      city.innerText = `${weather.name}, ${weather.sys.country}`;
+    
+      let now = new Date();
+      let date = document.querySelector(".location .date");
+      date.innerText = dateBuilder(now);
+    
+      let temp = document.querySelector(".current .temp");
+      temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+    
+      let weather_el = document.querySelector(".current .weather");
+      weather_el.innerText = weather.weather[0].main;
+    
+      let hilow = document.querySelector(".hi-low");
+      hilow.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`;
+    
+      let body = document.querySelector("body");
+    
+      if (weather.weather[0].main === "Sunny") {
+         body.style.backgroundImage = "url('https://images.unsplash.com/photo-1605371893234-db5e7b01aad2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')";
+      } else if (weather.weather[0].main === "Clouds") {
+        body.style.backgroundImage = "url('https://images.unsplash.com/photo-1646069816777-7e8a69b0596f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80')";
+      } else if (weather.weather[0].main === "Haze") {
+          body.style.backgroundImage = "url('https://images.unsplash.com/photo-1527666466760-f6a7a2b31176?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')";
+        }
+        else if (weather.weather[0].main === "Clear") {
+          body.style.backgroundImage = "url('https://images.unsplash.com/photo-1601297183305-6df142704ea2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80')";
+        }
+        
+      else if (weather.weather[0].main === "Rain") {
+        body.style.backgroundImage = "url('https://images.unsplash.com/photo-1429552054921-018e433d7d34?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1176&q=80')";
+      } else if (weather.weather[0].main === "Snow") {
+        body.style.backgroundImage = "url('https://images.unsplash.com/photo-1578410290083-b6ed52942332?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80')";
+      } else {
+        body.style.backgroundImage = "url('https://images.unsplash.com/photo-1526943604017-955071a1fb3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')";
+      }
+    }
+  
+  function dateBuilder(d) {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`;
+  }
+  
